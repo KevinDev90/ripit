8ad18a48bd899edf59abaf613e164fe19c7a2e4a
Kevin Guzman <guzmankevin90@gmail.com>
2023-11-15 16:15:13 -0500

    last fixes

--- src/services/openIAapi.js
+++ src/services/openIAapi.js
@@ -1,8 +1,6 @@
-import Constants from "expo-constants";
+import { API_KEY_OPENAI } from "../../apiKeys";
 
-const config = Constants.expoConfig.extra;
-
-export const apiKey = config.API_KEY_OPENAI;
+export const apiKey = API_KEY_OPENAI;
 export const urlCompetitions = "https://api.openai.com/v1/completions";
 export const urlChat = "https://api.openai.com/v1/chat/completions";
 export const urlImages = "https://api.openai.com/v1/images/generations";
